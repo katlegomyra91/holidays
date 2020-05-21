12fd28a9188bab646197de7659d4be648d77602a
katlegomyra91 <katlego@myramade.co.za>
2020-05-21 14:24:47 +0200

    Set up api routes

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+|------------|
+| API Routes |
+|------------|
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('holidays', 'HolidayController');
+Route::get('init', 'HolidayController@initHolidays');
